7cee485197c9ddfded381154f1e810d3564b4f6a
marwanrawshedh <marwansalem55@yahoo.com>
2021-09-12 18:35:19 +0300

    editing

---
+++ Forcastmovie.js
@@ -0,0 +1,11 @@
+function Forecastmovie(day) {
+    this.title = day.original_title,
+        this.overview = day.overview,
+        this.average_votes = day.vote_average,
+        this.total_votes = day.vote_count,
+        this.popularity = day.popularity,
+        this.released_on = day.release_date,
+        this.image=poster_path;
+}
+
+module.exports=Forecastmovie
