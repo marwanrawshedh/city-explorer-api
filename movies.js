a34e4943d4b4a81b99aaae7c69d559236ead2bbc
marwanrawshedh <marwansalem55@yahoo.com>
2021-09-12 16:24:47 +0300

    editing

--- movies.js
+++ movies.js
@@ -1,29 +1,47 @@
 let axios = require('axios')
 let moviesobj={}
+// function gettingmovies(req, res) {
+//     let name = req.query.name;
+//     if(moviesobj[name]==undefined){
+//         let urlmovies = `https://api.themoviedb.org/3/search/movie?query=${name}&api_key=${process.env.MOVIE_API_KEY}`;
+//     axios
+//         .get(urlmovies)
+//         .then(arrmovies => {
+//             let newmovie = arrmovies.data.results.map(item => {
+//                 return new Forecast(item);})
+//             moviesobj[name]=newmovie
+//             res.send(newmovie)})
+//         .catch(err => console.log(err))
+
+//     function Forecast(day) {
+//         this.title = day.original_title,
+//             this.overview = day.overview,
+//             this.average_votes = day.vote_average,
+//             this.total_votes = day.vote_count,
+//             this.popularity = day.popularity,
+//             this.released_on = day.release_date;}}
+
+//     else {res.send(moviesobj[name])}}
 function gettingmovies(req, res) {
-    
     let name = req.query.name;
-    if(moviesobj[name]==undefined){
-        let urlmovies = `https://api.themoviedb.org/3/search/movie?query=${name}&api_key=${process.env.MOVIE_API_KEY}`;
+    if(moviesobj[name]!==undefined){
+        res.send(moviesobj[name])}
+    else {let urlmovies = `https://api.themoviedb.org/3/search/movie?query=${name}&api_key=${process.env.MOVIE_API_KEY}`;
     axios
         .get(urlmovies)
         .then(arrmovies => {
             let newmovie = arrmovies.data.results.map(item => {
-                return new Forecast(item);
-            })
+                return new Forecast(item);})
             moviesobj[name]=newmovie
-            res.send(newmovie)
-        })
-
+            res.send(newmovie)})
         .catch(err => console.log(err))
+        
     function Forecast(day) {
         this.title = day.original_title,
             this.overview = day.overview,
             this.average_votes = day.vote_average,
             this.total_votes = day.vote_count,
             this.popularity = day.popularity,
-            this.released_on = day.release_date;
-    }}
-    else {res.send(moviesobj[name])}
-}
+            this.released_on = day.release_date;}}}
+            
 module.exports = gettingmovies
